b81edb976c6c08b82c9d4c0db6be71233f186983
vivupadi <vivupadi@gmail.com>
2024-12-14 00:30:59 +0100

    Change in GUI, added RAG based(need to be completed)

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from langchain.chains import RetrievalQA
 #from langchain.document_loaders import JSONLoader
-from langchain_community.document_loaders import JSONLoader
+from langchain_community.document_loaders import PyPDFLoader
+#from langchain_community.document_loaders import JSONLoader
 #from langchain.vectorstores import FAISS
 #from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -12,12 +13,23 @@ from langchain_community.llms import HuggingFacePipeline
 #from langchain.llms import HuggingFacePipeline
 from transformers import pipeline
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv('api_key')
+
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
 
+#Load the model
+API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+API_KEY = os.getenv('api_key')
+HEADERS = {"Authorization": f"Bearer {API_KEY}"}
+
 # Load your CV as a JSON document
-loader = JSONLoader(file_path="C:\\Users\\Vivupadi\\Desktop\\Portfolio\\Vivek_Resume.json", jq_schema=".")
+loader = PyPDFLoader(file_path="C:\\Users\\Vivupadi\\Desktop\\Portfolio\\Vivek Padayattil_CV_2024.pdf")
 documents = loader.load()
 
 # Create embeddings and vector store
@@ -35,7 +47,7 @@ qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriev
 def chat():
     data = request.json
     question = data.get('message', '')
-    response = qa_chain.run(question)
+    response = qa_chain.run({"query": question})
     return jsonify({"reply": response})
 
 if __name__ == '__main__':
