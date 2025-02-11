72fbe9902437d5f25af1d8cf85328f5e76ba4a96
vivupadi <vivupadi@gmail.com>
2025-02-12 00:53:41 +0100

    requirements files added

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,7 +1,7 @@
 async function sendMessage() {
     let userInput = document.getElementById("userInput").value;
     
-    let response = await fetch("https://vivupadi-github-io.onrender.com/chat", { 
+    let response = await fetch("http://127.0.0.1:5000/chat", { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userInput })
